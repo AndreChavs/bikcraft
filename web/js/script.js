e4952d0b986417b2619edcb0e7579753b38b08f4
AndreChavs <keysandr7@gmail.com>
2021-05-23 13:28:45 -0300

    Atualização 23-05
    ultima atualização

--- web/js/script.js
+++ web/js/script.js
@@ -1,21 +1,22 @@
-if (window.SimpleSlide) {
-    new SimpleSlide({
-        slide: 'quote', // nome do atributo data-slide="principal"    
-        auto: true, // se o slide deve passar automaticamente
-        time: 6000, // tempo de transição dos slides    
-    });
-    new SimpleSlide({
-        slide: 'portfolio', // nome do atributo data-slide="principal"
-        nav: true, // se deve ou não mostrar a navegação
-        auto: true, // se o slide deve passar automaticamente
-        time: 5000, // tempo de transição dos slides    
-    });
+if(window.SimpleSlide) {
+
+	new SimpleSlide({
+		slide: "quote", // nome do atributo data-slide="principal"
+	  time: 5000 // tempo de transição dos slides
+	});
+
+	new SimpleSlide({
+		slide: "portfolio",
+	  time: 5000,
+	  nav: true
+	});
+
 }
 
-if(window.SimpleAnime){
-    new SimpleAnime();
-    
+if(window.SimpleAnime) {
+	new SimpleAnime();
 }
+
 
 if(window.SimpleForm) {
 	new SimpleForm({
